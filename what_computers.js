a2f15acc424a92effae0142d051b67fa6edba6b1
Aaryan Pashine <pashineaaryan@gmail.com>
2025-09-21 00:25:47 -0400

    lovely cleanup?

--- what_computers.js
+++ what_computers.js
@@ -1,12 +1,48 @@
 let filters = {}
 let views = {}
 
+function isNode (el) {
+  return el && el.nodeName && el.nodeType
+}
+let dom = (el, ...contents) => {
+	let ell = "div"
+	let classes = []
+	let id = ""
+	let parseclass = (str) => {
+		let identifiers = str.split(/([\.#]?[^\s#.]+)/).map(e => e.trim()).filter(e => e!= "")
+		if(!(/^\.|#/.test(identifiers[0]))) {
+			ell = identifiers[0]
+			identifiers.shift()
+		}
+
+		identifiers.forEach(i => {
+			if(i[0] == ".") classes.push(i.slice(1))
+			if(i[0] == "#") id = i.slice(1)
+		})
+
+	}
+
+	parseclass(el)
+	let d = document.createElement(ell)
+
+	classes.forEach((c) => d.classList.add(c))
+	id ? d.id = id : null
+
+	contents.forEach((e) => {
+		if (typeof e == 'string') d.innerText += e
+		else if (isNode(e)) d.appendChild(e)
+		else if (typeof e == 'object'){
+			Object.entries(e).map(([k, v]) => {
+				d[k] = v
+			})
+		}
+	})
+
+	return d
+}
+
 filters.text = (block) => block.class == 'Text'
 views.text = (block) => {
-	let div = document.createElement("div")
-	div.classList.add("text")
-	div.classList.add("block")
-
 	let editorstate = true
 	let toggleeditor = () => {
 		editorstate = !editorstate
@@ -21,21 +57,13 @@ views.text = (block) => {
 			btn.innerText = 'edit'
 		}
 	}
+	let inner = dom("div", {innerHTML: block.content_html})
+	let editor = dom("textarea", {value: block.content})
 
-	let inner = document.createElement("div")
-	inner.innerHTML = block.content_html
-	div.appendChild(inner)
+	let div = dom(".text.block", editor, inner)
 
-	let editor = document.createElement("textarea")
-	editor.value = block.content
-	div.appendChild(editor)
-	
-	let btn = document.createElement("button")
-	btn.innerText = 'edit'
-	btn.onclick = () => toggleeditor()
-
-	if (localStorage.getItem("ME") == block.user.slug)
-	div.appendChild(btn)
+	let btn = document.createElement("button", {onclick: toggleeditor}, "edit")
+	if (localStorage.getItem("ME") == block.user.slug) div.appendChild(btn)
 
 	toggleeditor()
 
@@ -44,85 +72,39 @@ views.text = (block) => {
 
 filters.image = (block) => block.class == 'Image'
 views.image = (block) => {
-	let div = document.createElement("div")
-	div.classList.add("image")
-	div.classList.add("block")
-
-	let img = document.createElement("img")
-	img.src = block.image.display.url
-
-	div.appendChild(img)
+	let img = dom("img", {src: block.image.display.url})
+	let div = dom("div.image.block", img)
 	return div
 }
 
 filters.link = (block) => block.class == 'Link'
 views.link = (block) => {
-	let div = document.createElement("div")
-	div.classList.add("link")
-	div.classList.add("block")
-
-	let a = document.createElement("a")
-	a.href = block.source.url
-	a.target = "_blank"
-
-	let span = document.createElement("span")
-	span.textContent = block.title
-
-	a.appendChild(span)
-	div.appendChild(a)
+	let span = dom("span", block.title)
+	let a = dom("a", {href : block.source.url, target : "_blank"}, span)
+	let div = dom(".link.block", a)
 	return div
 }
 
 filters.channel = (block) => block.class == 'Channel'
 views.channel = (block) => {
-	let div = document.createElement("div")
-	div.classList.add("channel")
-	div.classList.add("block")
-
-	let a = document.createElement("a")
-	a.href = `../#!/${block.slug}`
-
-	let h4 = document.createElement("h4")
-	h4.textContent = block.title
-
-	a.appendChild(h4)
-	div.appendChild(a)
+	let a = dom("a", {href:  `../#!/${block.slug}`}, dom("h4", block.title))
+	let div = dom(".channel.block", a)
 	return div
 }
 
 filters.media = (block) => block.class == 'Media'
 views.media = (block) => {
-	let div = document.createElement("div")
-	div.classList.add("media")
-	div.classList.add("block")
-
-	let span = document.createElement("span")
-	span.classList.add("tag")
-	span.textContent = "media "
-
-	let h4 = document.createElement("h4")
-	h4.textContent = block.title
-
-	div.appendChild(span)
-	div.appendChild(h4)
+	let span = dom("span.tag", "media")
+	let h4 = dom("h4", block.title)
+	let div = dom(".media.block", span, h4)
 	return div
 }
 
 filters.attachment = (block) => block.class == 'Attachment'
 views.attachment = (block) => {
-	let div = document.createElement("div")
-	div.classList.add("attachment")
-	div.classList.add("block")
-
-	let span = document.createElement("span")
-	span.classList.add("tag")
-	span.textContent = "attachment "
-
-	let h4 = document.createElement("h4")
-	h4.textContent = block.title
-
-	div.appendChild(span)
-	div.appendChild(h4)
+	let span = dom("span.tag",  "attachment ")
+	let h4 = dom("h4", block.title)
+	let div = dom(".attachment.block", span, h4)
 	return div
 }
 export let what_computers = {filters, views}
